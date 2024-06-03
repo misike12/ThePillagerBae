e41576d7a39c8de94d0fff6fe5b234bc09bc9bb2
misike12 <59843249+misike12@users.noreply.github.com>
2024-06-03 19:49:44 +0200

    dsd

--- api/index.php
+++ api/index.php
@@ -1,2 +1,21 @@
 <?php
-phpinfo();
+
+$uri = "mysql://avnadmin:AVNS_Snor2TTMzlFMa0yadMT@mysql-1fce72c7-feleslegesfosfiok-821a.j.aivencloud.com:21526/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=defaultdb";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try {
+  $db = new PDO($conn, $fields["user"], $fields["pass"]);
+
+  $stmt = $db->query("SELECT VERSION()");
+  print($stmt->fetch()[0]);
+} catch (Exception $e) {
+  echo "Error: " . $e->getMessage();
+}
